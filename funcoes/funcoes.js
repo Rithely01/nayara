7331565b6dbc1e05740c9667d9864fc55c02fd5d
unknown <rithelynunes6213@digitalcollege.internal>
2025-05-20 21:55:42 -0300

    sammuel q fez

---
+++ funcoes/funcoes.js
@@ -0,0 +1,58 @@
+// const nome = prompt
+// function nomeDaFuncao (paramentro) {
+//     bloco de construcao
+// }
+// const nome = prompt("Digite seu nome")
+
+// function cumprimentar (pessoa) {
+//     alert(`Ola ${pessoa}`)
+// }
+
+// cumprimentar("Samuel")
+// cumprimentar("Mueceguinha")
+// cumprimentar("Myrela")
+// cumprimentar("Rithely")
+// cumprimentar(nome)
+
+//fnções podem receber retorno
+
+const numero1 = Number(prompt("Digite o primeiro número"))
+const numero2 = Number(prompt("Digite o segundo número"))
+
+
+
+// function somar (a,b) {
+//     const resultado = a + b;
+//     console.log(resultado)
+// }
+// somar(10, 5)  //15
+
+// function subtrair (a, b) {
+//     const resultado = a - b
+//     alert(´resultado´)
+// }
+// console.log(subtrair(10,3))
+
+//arrow function
+
+// const subtrair + (a,b) => {
+//     const resultado = a + b
+//     return resultado
+
+// }
+
+
+const numeroIput = Number (documento.getElementById('numeroIpunt').velue)
+const resultado = document.getElementById('resultado')
+
+f
+
+function tabuada() {
+
+   
+    for(let i = 1; i <= 10; i++) {
+        sainda +=`<p>${numeroIput} * ${i} = ${numeroIput * i}</p>`
+    }
+    resultado.innerHTML = saida
+}
+
